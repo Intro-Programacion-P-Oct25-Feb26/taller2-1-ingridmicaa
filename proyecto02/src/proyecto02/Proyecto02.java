8aabe587a64f49fb67087e606f73e8a5212887ad
ingridmicaa <ingridmicaela03@gmail.com>
2025-10-16 12:50:55 -0500

    Carrera nombre y ciclos correspondiente

--- proyecto02/src/proyecto02/Proyecto02.java
+++ proyecto02/src/proyecto02/Proyecto02.java
@@ -15,6 +15,7 @@ public class Proyecto02 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.println("Carrera Computación con 9 ciclos");
     }
     
 }
